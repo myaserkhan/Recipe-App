6579d7836c0c72150d5820ecc92746fe11eb245e
Khaled-Al-Khateeb <khaled.w.khateeb85@gmail.com>
2023-02-22 08:26:56 +0300

    Add create new inventory button

--- db/schema.rb
+++ db/schema.rb
@@ -10,62 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_201045) do
-    # These are extensions that must be enabled in order to support this database
-    enable_extension "plpgsql"
-  
-    create_table "foods", force: :cascade do |t|
-      t.string "name"
-      t.string "measurement_unit"
-      t.float "price"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-  
-    create_table "inventories", force: :cascade do |t|
-      t.string "name"
-      t.bigint "user_id", null: false
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.text "description"
-      t.index ["user_id"], name: "index_inventories_on_user_id"
-    end
-  
-    create_table "recipe_foods", force: :cascade do |t|
-      t.integer "quantity"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.bigint "recipe_id"
-      t.index ["recipe_id"], name: "index_recipe_foods_on_recipe_id"
-    end
-  
-    create_table "recipes", force: :cascade do |t|
-      t.string "name"
-      t.integer "preparation_time"
-      t.integer "cooking_time"
-      t.text "description"
-      t.boolean "public"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.bigint "user_id"
-      t.index ["user_id"], name: "index_recipes_on_user_id"
-    end
-  
-    create_table "users", force: :cascade do |t|
-      t.string "email", default: "", null: false
-      t.string "name", default: "", null: false
-      t.string "encrypted_password", default: "", null: false
-      t.string "reset_password_token"
-      t.datetime "reset_password_sent_at"
-      t.datetime "remember_created_at"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["email"], name: "index_users_on_email", unique: true
-      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    end
-  
-    add_foreign_key "inventories", "users"
-    add_foreign_key "recipe_foods", "recipes"
-    add_foreign_key "recipes", "users"
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_093752) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  create_table "inventories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+    t.index ["user_id"], name: "index_inventories_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "inventories", "users"
+end
